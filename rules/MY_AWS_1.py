99aa63e13f1f5978f3cb36670f29398527f052f0
Dan Jones <dan.rh.jones@gmail.com>
2022-05-18 14:36:09 +0100

    new one

---
+++ rules/MY_AWS_1.py
@@ -0,0 +1,21 @@
+from checkov.terraform.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckResult, CheckCategories
+
+class TagCheck(BaseResourceCheck):
+    def __init__(self):
+        name = "check tags"
+        checkov_id = "MY_AWS_1"
+        supported_resources = ['aws_s3_bucket']
+        categories = [CheckCategories.GENERAL_SECURITY]
+        super().__init__(name=name, id=checkov_id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+
+        if conf.get("tags", []):
+            env = conf["tags"][0].get("Environment", {})
+            if env in ["dg", "Programme"]:
+                return CheckResult.PASSED
+        return CheckResult.FAILED
+
+
+scanner = TagCheck()
